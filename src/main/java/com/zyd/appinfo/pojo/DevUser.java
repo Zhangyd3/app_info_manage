092ef54e28697fc20d36a084e773cd52df0d193d
Zhangyd3 <2427716854@qq.com>
2021-11-17 15:21:25 +0800

    添加pojo文件

--- src/main/java/com/zyd/appinfo/pojo/DevUser.java
+++ src/main/java/com/zyd/appinfo/pojo/DevUser.java
@@ -2,13 +2,12 @@ package com.zyd.appinfo.pojo;
 
 import java.io.Serializable;
 import java.util.Date;
-import lombok.Data;
 
 /**
  * dev_user
  * @author 
  */
-@Data
+
 public class DevUser implements Serializable {
     /**
      * 主键id
@@ -61,4 +60,88 @@ public class DevUser implements Serializable {
     private Date modifydate;
 
     private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDevcode() {
+        return devcode;
+    }
+
+    public void setDevcode(String devcode) {
+        this.devcode = devcode;
+    }
+
+    public String getDevname() {
+        return devname;
+    }
+
+    public void setDevname(String devname) {
+        this.devname = devname;
+    }
+
+    public String getDevpassword() {
+        return devpassword;
+    }
+
+    public void setDevpassword(String devpassword) {
+        this.devpassword = devpassword;
+    }
+
+    public String getDevemail() {
+        return devemail;
+    }
+
+    public void setDevemail(String devemail) {
+        this.devemail = devemail;
+    }
+
+    public String getDevinfo() {
+        return devinfo;
+    }
+
+    public void setDevinfo(String devinfo) {
+        this.devinfo = devinfo;
+    }
+
+    public Long getCreatedby() {
+        return createdby;
+    }
+
+    public void setCreatedby(Long createdby) {
+        this.createdby = createdby;
+    }
+
+    public Date getCreationdate() {
+        return creationdate;
+    }
+
+    public void setCreationdate(Date creationdate) {
+        this.creationdate = creationdate;
+    }
+
+    public Long getModifyby() {
+        return modifyby;
+    }
+
+    public void setModifyby(Long modifyby) {
+        this.modifyby = modifyby;
+    }
+
+    public Date getModifydate() {
+        return modifydate;
+    }
+
+    public void setModifydate(Date modifydate) {
+        this.modifydate = modifydate;
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
 }
